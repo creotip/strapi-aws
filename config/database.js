ab08166e94db420a25096a15fef5c770d95e2bf4
ruslanelishaev <ruslan@byondxr.com>
2022-07-07 14:35:48 +0300

    bump

--- config/database.js
+++ config/database.js
@@ -4,7 +4,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'sqlite',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", ""),
+      password: env("DATABASE_PASSWORD", ""),
     },
     useNullAsDefault: true,
   },
